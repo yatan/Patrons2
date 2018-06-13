f90b2b913658ec34537368b2becee426509c575e
Fran <yatan87@gmail.com>
2018-06-13 18:39:42 +0200

    Exercici 1 Final Builder

---
+++ src/practicapatrons2/Builder.java
@@ -0,0 +1,29 @@
+package practicapatrons2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author Fran
+ */
+public class Builder {
+    private final List<Figure> components  = new ArrayList<>();
+    private double x;
+    private double y;
+    
+    private Builder(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    
+    public Builder add(Figure fig){
+        components.add(fig);
+        return this;
+    }
+    
+    public Drawing build(){
+        return new Drawing(Collections.unmodifiableList(components), x, y);
+    }
+}
